36f21e0f71b60efa6bcc62a56ccb802f770c6d2b
SqYtCO <SqYtCO@users.noreply.github.com>
2018-02-12 12:10:03 +0100

    Update timewidget.cpp

--- timewidget.cpp
+++ timewidget.cpp
@@ -2,8 +2,10 @@
 
 TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 {
+	// set timer to 0
 	reset();
 
+	// setup style of hour
 	hour.setMinimumSize(100, 80);
 	hour.setTextInteractionFlags(Qt::NoTextInteraction);
 	hour.setAutoFillBackground(true);
@@ -11,6 +13,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 	hour.setFrameShape(QFrame::Panel);
 	hour.setFrameShadow(QFrame::Raised);
 	hour.setAlignment(Qt::AlignCenter);
+	// setup style of minutes
 	minutes.setMinimumSize(100, 80);
 	minutes.setTextInteractionFlags(Qt::NoTextInteraction);
 	minutes.setAutoFillBackground(true);
@@ -18,6 +21,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 	minutes.setFrameShape(QFrame::Panel);
 	minutes.setFrameShadow(QFrame::Raised);
 	minutes.setAlignment(Qt::AlignCenter);
+	// setup style of seconds
 	seconds.setMinimumSize(100, 80);
 	seconds.setTextInteractionFlags(Qt::NoTextInteraction);
 	seconds.setAutoFillBackground(true);
@@ -25,6 +29,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 	seconds.setFrameShape(QFrame::Panel);
 	seconds.setFrameShadow(QFrame::Raised);
 	seconds.setAlignment(Qt::AlignCenter);
+	// setup style of milliseconds
 	milliseconds.setMinimumSize(150, 80);
 	milliseconds.setTextInteractionFlags(Qt::NoTextInteraction);
 	milliseconds.setAutoFillBackground(true);
@@ -33,6 +38,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 	milliseconds.setFrameShadow(QFrame::Raised);
 	milliseconds.setAlignment(Qt::AlignCenter);
 
+	// setup separator character and style of the separators
 	for(int i = 0; i < 3; ++i)
 	{
 		separator[i].setText(":");
@@ -41,6 +47,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 		separator[i].setAlignment(Qt::AlignCenter);
 	}
 
+	// setup layout
 	layout.addWidget(&hour);
 	layout.addWidget(&separator[0]);
 	layout.addWidget(&minutes);
@@ -54,6 +61,7 @@ TimeWidget::TimeWidget(QWidget* parent) : QWidget(parent)
 
 void TimeWidget::resizeEvent(QResizeEvent* event)
 {
+	// increase / decrease font size on resizeevent
 	hour.setFont(QFont("times", hour.width() / 2));
 	minutes.setFont(QFont("times", minutes.width() / 2));
 	seconds.setFont(QFont("times", seconds.width() / 2));
@@ -63,22 +71,24 @@ void TimeWidget::resizeEvent(QResizeEvent* event)
 	{
 		separator[i].setFont(QFont("times", width() / 25, QFont::ExtraLight));
 	}
-
-	event->accept();
 }
 
 void TimeWidget::updateMS()
 {
+	// increase time by 1
 	time = time.addMSecs(1);
+	// display current time
 	hour.setText(QString::number(time.hour()));
-	minutes.setText(QString::number(time.minute() % 60));
-	seconds.setText(QString::number(time.second() % 60));
-	milliseconds.setText(QString::number(time.msec() % 1000));
+	minutes.setText(QString::number(time.minute()));
+	seconds.setText(QString::number(time.second()));
+	milliseconds.setText(QString::number(time.msec()));
 }
 
 void TimeWidget::reset()
 {
+	// set time to 0
 	time.setHMS(0, 0, 0);
+	// show 0:0:0:000
 	hour.setText("0");
 	minutes.setText("0");
 	seconds.setText("0");
