c8f81e667341261d329493c412ba41efd0d53ef4
SqYtCO <SqYtCO@users.noreply.github.com>
2018-02-12 17:08:29 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication app(argc, argv);
+    app.setWindowIcon(QIcon("icon/stopwatch-icon.png"));
 	MainWindow w;
 	w.show();
 
-	return a.exec();
+	return app.exec();
 }
